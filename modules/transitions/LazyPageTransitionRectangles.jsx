195b611bc799033d50c9fffb4b7bc1c5b36b415d
drivex store <300916779+drivex-gamestore@users.noreply.github.com>
2026-09-13 00:54:37 +0630

    Update LazyPageTransitionRectangles.jsx

--- modules/transitions/LazyPageTransitionRectangles.jsx
+++ modules/transitions/LazyPageTransitionRectangles.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import dynamic from 'next/dynamic';
 
 const PageTransitionRectangles = dynamic(
-  () => import('@components/transitions/PageTransitionRectangles').then((mod) => mod.PageTransitionRectangles),
+  () => import('@modules/transitions/PageTransitionRectangles').then((mod) => mod.PageTransitionRectangles),
   { ssr: false }
 );
 export function LazyPageTransitionRectangles() {
