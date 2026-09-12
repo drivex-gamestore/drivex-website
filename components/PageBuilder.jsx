c85705339b7d62401999ac153f10233d044529f7
drivex store <300916779+drivex-gamestore@users.noreply.github.com>
2026-09-12 07:57:15 +0630

    Update PageBuilder.jsx

--- components/PageBuilder.jsx
+++ components/PageBuilder.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+
 import HeroSection from '@sections/HeroSection';
 import CardsSection from '@sections/CardsSection';
 import AnimatedListSection from '@sections/AnimatedListSection';
 import FeaturedWorkSection from '@sections/FeaturedWorkSection';
 import IndexedGridSection from '@sections/IndexedGridSection';
 import AccordionSection from '@sections/AccordionSection';
+import ContentBlockSection from '@sections/ContentBlockSection';
 
 const SECTION_COMPONENTS = {
   heroSection: HeroSection,
